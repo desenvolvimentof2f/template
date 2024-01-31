1436c9f9f2b6d60d79d37cd905513462796f5001
alefdav <alefdav@gmail.com>
2024-01-31 11:31:17 -0300

    first commit

---
+++ header.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="pt">
+
+<head>
+
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="manifest" href="<?php bloginfo('template_url') ?>/manifest.json">
+
+  <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+  <header>
+
+  </header>
